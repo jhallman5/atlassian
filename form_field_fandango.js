debbdd96b4661abbbcaf392b2fbdce7c23b8ce59
John <jhallman5@gmail.com>
2018-07-16 07:39:01 -0700

    adds ng-pattern, updates error message span innerHTML

--- form_field_fandango.js
+++ form_field_fandango.js
@@ -4,7 +4,8 @@ document.querySelector('.half.right').remove()
 // Update the firstname div to represent a full name
 const FullNameInput = document.getElementById('firstName')
 FullNameInput.setAttribute('placeholder', 'Full name')
-FullNameInput.setAttribute('required pattern', '[a-zA-Z]+\s[a-zA-Z]+')
+FullNameInput.setAttribute('ng-pattern', '[a-zA-Z]+\s[a-zA-Z]+')
+FullNameInput.setAttribute('pattern', '[a-zA-Z]+\s[a-zA-Z]+')
 FullNameInput.classList.remove('.half')
 /* The half class defined width as 186px, the removed last name
 div had a left margin of 10px. 186 * 2 + 10 = 382 */
@@ -12,6 +13,15 @@ FullNameInput.style.width = '382px'
 
 FullNameInput.addEventListener('blur', (event) => {
   if(!RegExp(event.target.pattern).test(event.target.value)){
-    
+    event.target.style.border = '2px solid #d04437'
+    console.log('set')
   }
 })
+
+const fullNameErrorDiv = document.querySelector('p[ng-show="signupForm.firstName.$invalid"] span')
+fullNameErrorDiv.innerHTML = 'Please enter your full name spearated by a space'
+console.log(fullNameErrorDiv)
+// const fullNameErrorDiv = document.createElement('div')
+// fullNameErrorDiv.style.backgroundColor = 'blue'
+// fullNameErrorDiv.style.height = '100px'
+// fullNameErrorDiv.style.width = '100px'
