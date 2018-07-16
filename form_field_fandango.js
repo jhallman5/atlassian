61027f6681205ccdac92c455b40b1dd4d6113bba
John <jhallman5@gmail.com>
2018-07-16 14:16:18 -0700

    Removes Error styling after entering full name

--- form_field_fandango.js
+++ form_field_fandango.js
@@ -4,8 +4,9 @@ document.querySelector('.half.right').remove()
 // Update the firstname div to represent a full name
 const FullNameInput = document.getElementById('firstName')
 FullNameInput.setAttribute('placeholder', 'Full name')
+// TODO Form Validation!
+FullNameInput.setAttribute('ng-pattern', '[a-zA-Z]+ +[a-zA-Z]+')
 FullNameInput.setAttribute('pattern', '[a-zA-Z]+ +[a-zA-Z]+')
-FullNameInput.classList.remove('.half')
 /* The half class defined width as 186px, the removed last name
 div had a left margin of 10px. 186 * 2 + 10 = 382 */
 FullNameInput.style.width = '382px'
@@ -15,8 +16,12 @@ FullNameInput.addEventListener('blur', (event) => {
   if(!RegExp(event.target.pattern).test(event.target.value)){
     event.target.style.border = '2px solid #d04437'
     fullNameDiv.classList.remove('ng-hide')
+  }else {
+    fullNameDiv.classList.add('ng-hide')
+    event.target.style.border = null
   }
 })
 
-const fullNameErrorDiv = document.querySelector('p[ng-show="signupForm.firstName.$invalid"] span')
-fullNameErrorDiv.innerHTML = 'Please enter your full name spearated by a space'
+//Updates Error Message
+const fullNameErrorSpan = document.querySelector('p[ng-show="signupForm.firstName.$invalid"] span')
+fullNameErrorSpan.innerHTML = 'Please enter your full name spearated by a space'
