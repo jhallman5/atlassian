d51a1d48282293cef63a2e536779bd189964f0e2
John <jhallman5@gmail.com>
2018-07-15 20:45:13 -0700

    Adds pattern

--- form_field_fandango.js
+++ form_field_fandango.js
@@ -4,7 +4,14 @@ document.querySelector('.half.right').remove()
 // Update the firstname div to represent a full name
 const FullNameInput = document.getElementById('firstName')
 FullNameInput.setAttribute('placeholder', 'Full name')
+FullNameInput.setAttribute('required pattern', '[a-zA-Z]+\s[a-zA-Z]+')
 FullNameInput.classList.remove('.half')
 /* The half class defined width as 186px, the removed last name
 div had a left margin of 10px. 186 * 2 + 10 = 382 */
 FullNameInput.style.width = '382px'
+
+FullNameInput.addEventListener('blur', (event) => {
+  if(!RegExp(event.target.pattern).test(event.target.value)){
+    
+  }
+})
