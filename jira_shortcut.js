50ef48825d30f2d7b8484d64a92cc2e5f3565e49
John <jhallman5@gmail.com>
2018-07-16 20:11:09 -0700

    removes a querySelector

--- jira_shortcut.js
+++ jira_shortcut.js
@@ -1,7 +1,7 @@
 // Meant to be run on https://www.atlassian.com/
 const jiraDirect = document.createElement('a')
-const parentHero = document.querySelector('div .hero-text ')
 const otherProducts = document.querySelector('div .hero-text a')
+const parentHero = otherProducts.parentElement
 jiraDirect.setAttribute('href','/software/jira/try')
 jiraDirect.setAttribute('class', 'button blue-dark standard button green-solid')
 jiraDirect.textContent = 'Go agile with Jira for free'
