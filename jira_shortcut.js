8c2f78e51c95b09ffa5f8337ac35cdd00a83778c
John <jhallman5@gmail.com>
2018-07-16 22:25:41 -0700

    Removes parentElement variable

--- jira_shortcut.js
+++ jira_shortcut.js
@@ -1,9 +1,8 @@
 // Meant to be run on https://www.atlassian.com/
 const jiraDirect = document.createElement('a')
 const otherProducts = document.querySelector('div .hero-text a')
-const parentHero = otherProducts.parentElement
 jiraDirect.setAttribute('href','/software/jira/try')
 jiraDirect.setAttribute('class', 'button blue-dark standard button green-solid')
 jiraDirect.textContent = 'Go agile with Jira for free'
 otherProducts.textContent = 'or checkout our other products'
-parentHero.insertBefore(jiraDirect, otherProducts)
+otherProducts.parentElement.insertBefore(jiraDirect, otherProducts)
