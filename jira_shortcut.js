4ea117be12ff109cd0541c50c72d119522dec288
John Hallman <john_hallman@apple.com>
2018-07-18 14:09:37 -0700

    updates otherProducts text content

--- jira_shortcut.js
+++ jira_shortcut.js
@@ -1,5 +1,5 @@
 // Meant to be run on https://www.atlassian.com/
-// Lines 3 - 9 can be used on atlassian.com when the summit banner is not present. 
+// Lines 3 - 9 can be used on atlassian.com when the summit banner is not present.
 // const jiraDirect = document.createElement('a')
 // const otherProducts = document.querySelector('div .hero-text a')
 // jiraDirect.setAttribute('href','/software/jira/try')
@@ -14,5 +14,5 @@ const otherProducts = document.querySelector('div .colspan-2 a[href="/try"]')
 jiraDirect.setAttribute('href','/software/jira/try')
 jiraDirect.setAttribute('class', 'button blue-dark standard button green-solid')
 jiraDirect.textContent = 'Go agile with Jira for free'
-otherProducts.textContent = 'or checkout our other products'
+otherProducts.textContent = 'Checkout our other products'
 otherProducts.parentElement.insertBefore(jiraDirect, otherProducts)
