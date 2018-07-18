dc62624a49ea78d5cf1dbbb7b9130456d7eba32f
John Hallman <john_hallman@apple.com>
2018-07-18 08:24:13 -0700

    accounts for atlassian summit banner update

--- jira_shortcut.js
+++ jira_shortcut.js
@@ -1,6 +1,16 @@
 // Meant to be run on https://www.atlassian.com/
+// Lines 3 - 9 can be used on atlassian.com when the summit banner is not present. 
+// const jiraDirect = document.createElement('a')
+// const otherProducts = document.querySelector('div .hero-text a')
+// jiraDirect.setAttribute('href','/software/jira/try')
+// jiraDirect.setAttribute('class', 'button blue-dark standard button green-solid')
+// jiraDirect.textContent = 'Go agile with Jira for free'
+// otherProducts.textContent = 'or checkout our other products'
+// otherProducts.parentElement.insertBefore(jiraDirect, otherProducts)
+
+// Updated to compensate for the addition of atlassian summit banner
 const jiraDirect = document.createElement('a')
-const otherProducts = document.querySelector('div .hero-text a')
+const otherProducts = document.querySelector('div .colspan-2 a[href="/try"]')
 jiraDirect.setAttribute('href','/software/jira/try')
 jiraDirect.setAttribute('class', 'button blue-dark standard button green-solid')
 jiraDirect.textContent = 'Go agile with Jira for free'
