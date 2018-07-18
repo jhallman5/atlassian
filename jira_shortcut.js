f7e924d143066c3dc83b96333ea090fb233423c7
John Hallman <john_hallman@apple.com>
2018-07-18 16:41:50 -0700

    Makes the differnce between the two scenarios one line

--- jira_shortcut.js
+++ jira_shortcut.js
@@ -1,16 +1,10 @@
 // Meant to be run on https://www.atlassian.com/
-// Lines 3 - 9 can be used on atlassian.com when the summit banner is not present.
-// const jiraDirect = document.createElement('a')
-// const otherProducts = document.querySelector('div .hero-text a')
-// jiraDirect.setAttribute('href','/software/jira/try')
-// jiraDirect.setAttribute('class', 'button blue-dark standard button green-solid')
-// jiraDirect.textContent = 'Go agile with Jira for free'
-// otherProducts.textContent = 'or checkout our other products'
-// otherProducts.parentElement.insertBefore(jiraDirect, otherProducts)
+// Original submission with no summit banner
+// const otherProducts = document.querySelector('div .hero-text a')\
 
 // Updated to compensate for the addition of atlassian summit banner
-const jiraDirect = document.createElement('a')
 const otherProducts = document.querySelector('div .colspan-2 a[href="/try"]')
+const jiraDirect = document.createElement('a')
 jiraDirect.setAttribute('href','/software/jira/try')
 jiraDirect.setAttribute('class', 'button blue-dark standard button green-solid')
 jiraDirect.textContent = 'Go agile with Jira for free'
